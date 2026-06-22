0934fcfabaa4ee7d8a69c1bafec1a665e09eacb8
SixxCodes <z.madayag.141776.tc@umindanao.edu.ph>
2026-06-23 00:12:02 +0800

    change for deploy.

--- app.py
+++ app.py
@@ -49,4 +49,7 @@ def add_header(response):
 if __name__ ==   "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    
+    host = os.environ.get("HOST", "0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host=host, port=port, debug=True)
